bf4cadf3ba7a2a12f015c990c943f811a3d3a1ad
Magesh Kuppan <tkmagesh77@gmail.com>
2021-06-16 12:55:04 +0530

    Updates

--- bug-tracker-app/src/app/bugs/bugs.component.ts
+++ bug-tracker-app/src/app/bugs/bugs.component.ts
@@ -25,4 +25,33 @@ export class BugsComponent{
     onRemoveClick(bugToRemove : Bug){
         this.bugs.splice(this.bugs.indexOf(bugToRemove), 1)
     }
+
+    onBugNameClick(bugToToggle : Bug){
+        bugToToggle.isClosed = !bugToToggle.isClosed;
+    }
+
+    onRemoveClosedClick(){
+
+        /* 
+        for (let index = this.bugs.length-1; index >= 0; index--){
+            if (this.bugs[index].isClosed)
+                this.bugs.splice(index, 1)
+        }  
+        */
+       
+        this.bugs = this.bugs.filter(bug => !bug.isClosed)
+    }
+
+    getClosedCount() : number {
+        /* 
+        let closedCount = 0;
+        for (let bug of this.bugs){
+            if (bug.isClosed)
+                closedCount++
+        }
+        return closedCount; 
+        */
+
+        return this.bugs.reduce((result, bug) => bug.isClosed ? result + 1 : result, 0)
+    }
 }
