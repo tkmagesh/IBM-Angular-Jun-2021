a9988fce1f36972c0d516196a20a6f828973e2bc
Magesh Kuppan <tkmagesh77@gmail.com>
2021-06-18 16:45:50 +0530

    routing demo

---
+++ bug-tracker-app-routing/src/app/notFound.component.ts
@@ -0,0 +1,12 @@
+import { Component } from "@angular/core";
+
+@Component({
+    selector : 'app-not-found',
+    template : `
+        <h3>Invalid URL</h3>
+        <p>If you see this contact the administrator</p>
+    `
+})
+export class NotFoundComponent{
+
+}
