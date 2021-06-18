fda639f405afea972414d54c06f8f8983fdc356a
Magesh Kuppan <tkmagesh77@gmail.com>
2021-06-18 10:36:00 +0530

    async using promises

--- async-demo/async-demos.js
+++ async-demo/async-demos.js
@@ -32,4 +32,29 @@
 
     window['addAsyncClient'] = addAsyncClient;
 
+    function addAsyncPromise(x,y){
+        console.log(`   [@service] processing ${x} and ${y}`)
+        const p = new Promise(function(resolveFn, rejectFn){
+            setTimeout(function(){
+                let result = x + y;
+                console.log(`   [@service] returning result`)
+                resolveFn(result)
+            },3000);
+        })
+        return p;
+    }
+
+    window['addAsyncPromise'] = addAsyncPromise;
+
+    function addAsyncPromiseClient(x,y){
+        console.log(`[@client] invoking the service`);
+        const p = addAsyncPromise(100,200);
+        //then(callback), catch(callback)
+        p.then(function(result){
+            console.log(`[@client] result = ${result}`);
+        })
+    }
+
+    window['addAsyncPromiseClient'] = addAsyncPromiseClient
+
 })()
